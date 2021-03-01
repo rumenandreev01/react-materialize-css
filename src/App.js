50a5f75679e0f7217064c39e1fa1673f28194520
rumenandreev01 <rumenandreev@mail.bg>
2021-03-01 10:31:07 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Modal from './components/layout/Modal';
+import Navbar from './components/layout/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Modal/>      
+    </div>
+  );
+}
+
+export default App;
